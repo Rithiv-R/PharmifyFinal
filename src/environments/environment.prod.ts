b2a41f5b45bebe1705282ff59a30e39734a86250
Rithiv <rithiv07@gmail.com>
2022-04-25 00:07:21 +0530

    deploy1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAin0rSUlW6xOJBcTRZhYaA8fCDIdHJfP0",
+    authDomain: "pharmify-b3dab.firebaseapp.com",
+    projectId: "pharmify-b3dab",
+    storageBucket: "pharmify-b3dab.appspot.com",
+    messagingSenderId: "1068224675444",
+    appId: "1:1068224675444:web:6b36e731b83bc11e6ddb21",
+    measurementId: "G-R3J93KTCG1"
+  },
 };
